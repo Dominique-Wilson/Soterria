4045812ce46c2c0b074ee59c14921d2509b52ce6
Dominique W <git@dominiquewilson.xyz>
2024-05-19 00:22:55 -0500

    added slime with idle, move, and death animations

--- Scripts/arrow.gd
+++ Scripts/arrow.gd
@@ -11,6 +11,8 @@ func _process(delta):
 	#change position of arrow (move speed +) and infinite speed as above
 	position += (Vector2.RIGHT*speed).rotated(rotation) * delta
 
+func arrow_deal_damage():
+	pass
 
 func _on_visible_on_screen_enabler_2d_screen_exited():
 	queue_free()
